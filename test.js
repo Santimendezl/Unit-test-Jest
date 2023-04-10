a6f7b20e46fd9308e6451225e138ecfad79c82b5
Santimendezl <santimlacosta@gmail.com>
2023-04-10 17:55:36 +0000

    Final

--- test.js
+++ test.js
@@ -6,14 +6,14 @@ const {
 } = require('./app.js');
 
 //pruebas de test
-test("One euro should be 1.20 dollars", function() {
+test("One euro should be 1.20 dollars", () => {
     expect(fromEuroToDollar(3.5)).toBe(4.20);
 })
 
-test("One dollar should be 106.58 yens", function() {
+test("One dollar should be 106.58 yens", () => {
     expect(fromDollarToYen(1.81)).toBe(192.91);
 })
 
-test("One yen should be 0.006 pounds", function() {
+test("One yen should be 0.006 pounds", () => {
     expect(FromYenToPound(624)).toBe(3.74);
 })
